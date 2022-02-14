c81e286a8145088c34c1df161ea2e123367bfc12
Muhammad Hazrat <221441166@formanite.fccollege.edu.pk>
2022-02-14 12:21:15 +0500

    Update sell_medicine.php

--- medicine/sell_medicine.php
+++ medicine/sell_medicine.php
@@ -32,17 +32,37 @@
 <form class="col-md-4 col-md-offset-4 signUpForm" id="admin_profile_form">
     <label for="NameofMedicine">Enter The Name Of The Medicine You Want To Sell: </label>
     <input type="text" name="NameofMedicine" id="NameofMedicine">
-    <input type="submit" name="bNameofMedicine" value="Verify Medicine Exists">
 
     <h3>Current Quantity: <?php echo "19" ?></h3>
 
 
     <label for="quantity">Quantity To Sell: </label>
     <input type="number" name="quantity" id="quantity">
-    <input type="submit" name="updateQuantity" value="Update Quantity">
+    <input type="submit" name="bAddToCart" value="Add To Cart">
+
+    <input type="submit" name="checkOut" value="Check Out">
 
 </form>
 
+<?php
+    include ("../Database/database.php");
+
+    $conn = OpenCon();
+
+    if (isset($_POST["bAddToCart"])) {
+        $NameofMedicine = $_POST["NameofMedicine"];
+        $quantity = $_POST["quantity"];
+
+//        Trying to verify if medicine exists and then minus the qty from it
+//        Then, echo medicine added to cart. with same receipt id.
+//        If checkout clicked, it will made a whole receipt.
+
+
+    }
+
+    CloseCon($conn);
+
+?>
 
 
 </body>
